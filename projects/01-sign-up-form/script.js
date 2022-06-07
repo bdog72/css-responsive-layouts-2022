8994658e149669058c8bd0f6e782d4a8b3b57fa2
Brian Bycynski <high5bri@hotmail.com>
2022-06-07 03:07:32 -0400

    working on home page

--- projects/01-sign-up-form/script.js
+++ projects/01-sign-up-form/script.js
@@ -2,3 +2,10 @@
 //
 
 console.log(123);
+
+const container = document.querySelector('.container');
+const signUpBtn = document.querySelector('.green-bg button');
+
+signUpBtn.addEventListener('click', () => {
+  container.classList.toggle('change');
+});
