be534d3af5f459e5870caa1730630a5d48ed0214
Brian Bycynski <high5bri@hotmail.com>
2022-06-09 03:33:59 -0400

    3d swing

--- projects/01-sign-up-form/script.js
+++ projects/01-sign-up-form/script.js
@@ -4,8 +4,13 @@
 console.log(123);
 
 const container = document.querySelector('.container');
-const signUpBtn = document.querySelector('.green-bg button');
+const signUpBtn1 = document.querySelector('.sign-up-btn1');
+const signUpBtn2 = document.querySelector('.sign-up-btn2');
 
-signUpBtn.addEventListener('click', () => {
+signUpBtn1.addEventListener('click', () => {
+  container.classList.toggle('change');
+});
+
+signUpBtn2.addEventListener('click', () => {
   container.classList.toggle('change');
 });
--- projects/02-nike-product-card/script.js
+++ projects/02-nike-product-card/script.js
@@ -1,3 +1,15 @@
 //
 //
 console.log(123);
+
+const container = document.querySelector('.container');
+const color1 = document.querySelector('.color-1');
+const color2 = document.querySelector('.color-2');
+
+color2.addEventListener('click', () => {
+  container.classList.add('change');
+});
+
+color1.addEventListener('click', () => {
+  container.classList.remove('change');
+});
