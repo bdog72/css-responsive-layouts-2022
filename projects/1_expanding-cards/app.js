385ccab7e57bcc80fdf37c16e881057973a0c323
Brian Bycynski <high5bri@hotmail.com>
2022-08-20 05:00:38 -0400

    expanding cards

--- projects/1_expanding-cards/app.js
+++ projects/1_expanding-cards/app.js
@@ -1,4 +1,32 @@
 //
 //
 
-console.log('Template');
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClass();
+    panel.classList.add('active');
+  });
+});
+
+const removeActiveClass = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+};
+
+// const panels = document.querySelectorAll('.panel');
+
+// panels.forEach((panel) => {
+//   panel.addEventListener('click', () => {
+//     removeActiveClasses();
+//     panel.classList.add('active');
+//   });
+// });
+
+// const removeActiveClasses = () => {
+//   panels.forEach((panel) => {
+//     panel.classList.remove('active');
+//   });
+// };
